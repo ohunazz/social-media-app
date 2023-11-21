b66548766061983025267575117bbf46ad2296e0
ohunazz <ohunazz@gmail.com>
2023-11-21 10:38:08 -0500

    Add Login Part

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,5 +6,6 @@ import { GlobalError } from "../middlewares/global-error.middleware.js";
 const userRouter = Router();
 
 userRouter.post("/sign-up", userController.signUp);
+userRouter.post("/login", userController.login);
 
 export { userRouter };
