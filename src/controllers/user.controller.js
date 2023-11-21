cebd4d45cf6263f876d2d1ce8b498d362797f061
ohunazz <ohunazz@gmail.com>
2023-11-21 10:02:27 -0500

    Add Middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,2 +1,23 @@
 import { userService } from "../services/user.service.js";
 import { catchAsync } from "../utils/catch-async.js";
+
+class UserController {
+    signUp = catchAsync(async (req, res) => {
+        const { body } = req;
+
+        const userInput = {
+            email: body.email,
+            firstName: body.firstName,
+            lastName: body.lastName,
+            dateOfBirth: body.dateOfBirth,
+            password: body.password
+        };
+
+        await userService.signUp(userInput);
+        res.status(201).json({
+            message: "Success"
+        });
+    });
+}
+
+export const userController = new UserController();
