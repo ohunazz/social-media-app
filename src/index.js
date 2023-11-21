41c46c6d59d78b3a9e0e1da05d62a7e20083e4d7
ohunazz <ohunazz@gmail.com>
2023-11-21 10:34:29 -0500

    Finish Sign Up Part

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { userRouter } from "./routes/user.route.js";
+import { GlobalError } from "./middlewares/global-error.middleware.js";
 
 dotenv.config();
 
@@ -7,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
+app.use("/user", userRouter);
+app.use(GlobalError.handle);
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
---
+++ src/routes/user.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { userController } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { GlobalError } from "../middlewares/global-error.middleware.js";
+
+const userRouter = Router();
+
+userRouter.post("/sign-up", userController.signUp);
+
+export { userRouter };
